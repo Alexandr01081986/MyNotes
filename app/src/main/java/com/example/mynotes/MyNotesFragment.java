ee8e96d3f9283c93dfcddbb3026b5e386b32d3e3
Alexandr01081986 <bitkov_xgold@mail.ru>
2021-06-18 07:07:41 +0300

    Приложение заметки lesson6

---
+++ app/src/main/java/com/example/mynotes/MyNotesFragment.java
@@ -0,0 +1,72 @@
+package com.example.mynotes;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mynotes.model.ListNote;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class MyNotesFragment extends Fragment {
+
+
+    private static final String ARG_COLUMN_COUNT = "column-count";
+
+    private int mColumnCount = 1;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MyNotesFragment() {
+    }
+
+
+    @SuppressWarnings("unused")
+    public static MyNotesFragment newInstance(int columnCount) {
+        MyNotesFragment fragment = new MyNotesFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.node_list, container, false);
+
+        ListNote notes = ListNote.getInstance();
+
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new NotesRecyclerViewAdapter(notes.toListNode(), this));
+        }
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/mynotes/NoteDetailsActivity.java
@@ -0,0 +1,32 @@
+package com.example.mynotes;
+
+import android.content.res.Configuration;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            finish();
+            return;
+        }
+        if (savedInstanceState == null) {
+
+            NoteDetailsFragment details = new NoteDetailsFragment();
+            details.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.nodeDetailsContainer, details)
+                    .commit();
+        }
+    }
+
+}
